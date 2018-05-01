b2c5cd7a043bc885bb1e47076e0373f293ada979
Piotr Slowikowski <piotrslowikowski@wp.pl>
2018-05-01 13:31:45 +0200

    header and services section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    //submenu
+
     var menuItems = document.querySelectorAll('.menu ul li');
 
     var subMenu = document.querySelector('.subMenu');
@@ -17,4 +19,54 @@ document.addEventListener('DOMContentLoaded', function () {
             subMenu.classList.add('hidden');
         })
     }
+
+
+
+    //slider
+
+    var arrowLeft = document.querySelector('#arrowLeft');
+    var arrowRight = document.querySelector('#arrowRight');
+    var headerContent = document.querySelectorAll('.headerContent');
+    var counter = 0;
+
+    headerContent[counter].classList.remove('hidden');
+
+    arrowRight.addEventListener('click', function () {
+        headerContent[counter].classList.add('hidden');
+        counter++;
+        if (counter > 2) {
+            counter = 0;
+        }
+        headerContent[counter].classList.remove('hidden');
+
+    })
+
+    arrowLeft.addEventListener('click', function () {
+        headerContent[counter].classList.add('hidden');
+        counter--;
+        if (counter < 0) {
+            counter = 2;
+        }
+        headerContent[counter].classList.remove('hidden');
+
+    })
+
+
+    //hover
+
+    var thumb = document.querySelectorAll('.thumbnail');
+    var caption = document.querySelectorAll('.caption');
+
+    for (var i = 0; i < 2; i++) {
+        thumb[i].addEventListener('mouseover', function () {
+
+            this.firstElementChild.classList.add('hidden');
+        })
+
+        thumb[i].addEventListener('mouseleave', function () {
+
+            this.firstElementChild.classList.remove('hidden');
+        })
+    }
+
 })
